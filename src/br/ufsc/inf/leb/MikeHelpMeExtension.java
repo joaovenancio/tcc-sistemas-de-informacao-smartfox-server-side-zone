663bb95ecd7b99abe31eb643e55b14f0c3edc14f
Venâncio <joaovitorvenancio99@outlook.com>
2021-07-27 17:18:49 -0300

    Initial commit
    Initial commit

---
+++ src/br/ufsc/inf/leb/MikeHelpMeExtension.java
@@ -0,0 +1,29 @@
+package br.ufsc.inf.leb;
+
+import com.smartfoxserver.v2.core.ISFSEvent;
+import com.smartfoxserver.v2.core.SFSEventParam;
+import com.smartfoxserver.v2.core.SFSEventType;
+import com.smartfoxserver.v2.entities.Room;
+import com.smartfoxserver.v2.entities.variables.RoomVariable;
+import com.smartfoxserver.v2.entities.variables.SFSRoomVariable;
+import com.smartfoxserver.v2.exceptions.SFSException;
+import com.smartfoxserver.v2.extensions.BaseServerEventHandler;
+import com.smartfoxserver.v2.extensions.SFSExtension;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+public class MikeHelpMeExtension extends SFSExtension {
+
+    @Override
+    public void init() {
+        //General configuration:
+        getApi().getSystemScheduler().resizeThreadPool(10);
+        //Event Handlers:
+        this.addEventHandler(SFSEventType.ROOM_ADDED, RoomAddedEventHandler.class);
+        //Custom Requests Handlers:
+        //this.addRequestHandler("start", StartHandler.class);
+    }
+
+}
---
+++ src/br/ufsc/inf/leb/RoomAddedEventHandler.java
@@ -0,0 +1,34 @@
+package br.ufsc.inf.leb;
+
+import com.smartfoxserver.v2.core.ISFSEvent;
+import com.smartfoxserver.v2.core.SFSEventParam;
+import com.smartfoxserver.v2.entities.Room;
+import com.smartfoxserver.v2.entities.variables.RoomVariable;
+import com.smartfoxserver.v2.entities.variables.SFSRoomVariable;
+import com.smartfoxserver.v2.exceptions.SFSException;
+import com.smartfoxserver.v2.extensions.BaseServerEventHandler;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+public class RoomAddedEventHandler extends BaseServerEventHandler {
+    @Override
+    public void handleServerEvent(ISFSEvent isfsEvent) throws SFSException {
+        // reference to the created room
+        Room room = (Room) isfsEvent.getParameter(SFSEventParam.ROOM);
+
+        RoomVariable turn = new SFSRoomVariable("turn", "LOBBY");
+        turn.setGlobal(true);
+        turn.setPrivate(true);
+
+        RoomVariable timer = new SFSRoomVariable("timer", 0);
+        timer.setPrivate(true);
+
+        String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+        RoomVariable date = new SFSRoomVariable("date", timeStamp );
+        date.setPrivate(true);
+
+        getApi().setRoomVariables(null, room, Arrays.asList(turn, timer, date));
+    }
+}
